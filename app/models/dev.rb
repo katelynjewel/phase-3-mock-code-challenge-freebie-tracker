68f2470031bb2bcba88e5635319196ec1852749e
Katelyn <katelynjewel@gmail.com>
2021-11-09 14:08:49 -0800

    did the thing

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        "#{freebie.item_name}" ? ture : false 
+    end
+
+    def give_away dev, freebie
+        binding.pry
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder_mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+pop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "T-shirt", value: 20, company_id: google.id, dev_id: rick.id )
+Freebie.create(item_name: "Mug", value: 16, company_id: facebook.id, dev_id: morty.id)
+Freebie.create(item_name: "Sticker", value: 5, company_id: dunder_mifflin.id, dev_id: meseeks.id)
+Freebie.create(item_name: "Blanket", value: 24, company_id: enron.id, dev_id: pop.id)
+20.times do
+    Freebie.create(
+        item_name: Faker::ElectricalComponents.active, 
+        value: rand(1-20), 
+        company_id: Company.all.sample.id, 
+        dev_id: Dev.all.sample.id
+    )
+end
 
 puts "Seeding done!"
